3b0936df0154d1357c3413af374bfe9dddac626f
Violeta Hernández <vi.hdz.p@gmail.com>
2025-02-25 02:00:28 -0600

    fix?

--- CombinatorialGames/Game/Impartial.lean
+++ CombinatorialGames/Game/Impartial.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Fox Thomson
 -/
 import CombinatorialGames.Game.Basic
+import CombinatorialGames.Game.Specific
 import Mathlib.Tactic.NthRewrite
 
 /-!
