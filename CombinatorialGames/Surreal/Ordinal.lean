6d356932933a63c7e304b8d78a4eeb15355b5d8a
Violeta Hernández <vi.hdz.p@gmail.com>
2025-02-24 23:22:46 -0600

    fix PR

--- CombinatorialGames/Surreal/Ordinal.lean
+++ CombinatorialGames/Surreal/Ordinal.lean
@@ -3,8 +3,8 @@ Copyright (c) 2022 Violeta Hernández Palacios. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Violeta Hernández Palacios
 -/
-import Mathlib.SetTheory.Game.Ordinal
-import Mathlib.SetTheory.Surreal.Basic
+import CombinatorialGames.Game.Ordinal
+import CombinatorialGames.Surreal.Basic
 
 /-!
 # Surreals as games
