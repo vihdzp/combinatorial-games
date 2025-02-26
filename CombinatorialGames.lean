54f9f04d2f27dd85a900b1e80e07a04177d6d804
Violeta Hernández <vi.hdz.p@gmail.com>
2025-02-26 04:08:39 -0600

    lake exe mk_all

--- CombinatorialGames.lean
+++ CombinatorialGames.lean
@@ -5,6 +5,7 @@ import CombinatorialGames.Game.Concrete
 import CombinatorialGames.Game.Impartial
 import CombinatorialGames.Game.Ordinal
 import CombinatorialGames.Game.PGame
+import CombinatorialGames.Game.Relabelling
 import CombinatorialGames.Game.Short
 import CombinatorialGames.Game.Special
 import CombinatorialGames.Game.Specific.Domineering
