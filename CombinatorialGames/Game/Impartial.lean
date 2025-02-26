fe4e683da227bedd0478dd5e9e4ea7e67830e745
Violeta Hernández <vi.hdz.p@gmail.com>
2025-02-26 02:10:10 -0600

    chore: replace `fun _ => _` by `fun _ ↦ _` (#20)

--- CombinatorialGames/Game/Impartial.lean
+++ CombinatorialGames/Game/Impartial.lean
@@ -37,7 +37,7 @@ class Impartial (G : PGame) : Prop where
   out : ImpartialAux G
 
 private theorem impartial_iff_aux {G : PGame} : G.Impartial ↔ G.ImpartialAux :=
-  ⟨fun h => h.1, fun h => ⟨h⟩⟩
+  ⟨fun h ↦ h.1, fun h ↦ ⟨h⟩⟩
 
 theorem impartial_def {G : PGame} :
     G.Impartial ↔ G ≈ -G ∧ (∀ i, Impartial (G.moveLeft i)) ∧ ∀ j, Impartial (G.moveRight j) := by
@@ -74,13 +74,13 @@ instance moveRight_impartial {G : PGame} [h : G.Impartial] (j : G.RightMoves) :
 theorem impartial_congr {G H : PGame} (e : G ≡r H) [G.Impartial] : H.Impartial :=
   impartial_def.2
     ⟨e.symm.equiv.trans ((neg_equiv_self G).trans (neg_equiv_neg_iff.2 e.equiv)),
-      fun i => impartial_congr (e.moveLeftSymm i), fun j => impartial_congr (e.moveRightSymm j)⟩
+      fun i ↦ impartial_congr (e.moveLeftSymm i), fun j ↦ impartial_congr (e.moveRightSymm j)⟩
 termination_by G
 
 instance impartial_add (G H : PGame) [G.Impartial] [H.Impartial] : (G + H).Impartial := by
   rw [impartial_def]
   refine ⟨(add_congr (neg_equiv_self G) (neg_equiv_self _)).trans
-      (negAddRelabelling _ _).equiv.symm, fun k => ?_, fun k => ?_⟩
+      (negAddRelabelling _ _).equiv.symm, fun k ↦ ?_, fun k ↦ ?_⟩
   · apply leftMoves_add_cases k
     all_goals
       intro i; simp only [add_moveLeft_inl, add_moveLeft_inr]
@@ -93,7 +93,7 @@ termination_by (G, H)
 
 instance impartial_neg (G : PGame) [G.Impartial] : (-G).Impartial := by
   rw [impartial_def]
-  refine ⟨?_, fun i => ?_, fun i => ?_⟩
+  refine ⟨?_, fun i ↦ ?_, fun i ↦ ?_⟩
   · rw [neg_neg]
     exact (neg_equiv_self G).symm
   · rw [moveLeft_neg]
