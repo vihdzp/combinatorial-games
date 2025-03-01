55c5590961214d380434898e21b8dbb387100558
Violeta Hernández <vi.hdz.p@gmail.com>
2025-02-28 23:23:44 -0600

    add credits

--- CombinatorialGames/IGame/IGame.lean
+++ CombinatorialGames/IGame/IGame.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2025 Violeta Hernández Palacios. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Violeta Hernández Palacios
+Authors: Violeta Hernández Palacios, Reid Barton, Mario Carneiro, Isabel Longbottom, Kim Morrison, Yuyang Zhao
 -/
 import Mathlib.Algebra.Group.Pointwise.Set.Basic
 import Mathlib.Logic.Hydra
