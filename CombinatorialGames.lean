1b648cdbeab7ae3eb8725562b52df3c1ff3837d3
Tristan F.-R. <LeoDog896@hotmail.com>
2025-03-01 16:07:57 -0800

    chore: drop Game.State (#39)
    Co-authored-by: Violeta Hernández <vi.hdz.p@gmail.com>

--- CombinatorialGames.lean
+++ CombinatorialGames.lean
@@ -11,7 +11,6 @@ import CombinatorialGames.Game.Special
 import CombinatorialGames.Game.Specific.Domineering
 import CombinatorialGames.Game.Specific.Nim
 import CombinatorialGames.Game.Specific.Poset
-import CombinatorialGames.Game.State
 import CombinatorialGames.IGame.Basic
 import CombinatorialGames.IGame.IGame
 import CombinatorialGames.IGame.Special
