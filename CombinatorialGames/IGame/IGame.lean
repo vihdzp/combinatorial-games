eea35c6fafabc3ceec57292e310244a29a7581cc
Violeta Hernández <vi.hdz.p@gmail.com>
2025-03-08 18:30:15 -0600

    feat: `IGame.nim` (#62)
    Co-authored-by: Tristan F.-R. <LeoDog896@hotmail.com>

--- CombinatorialGames/IGame/IGame.lean
+++ CombinatorialGames/IGame/IGame.lean
@@ -289,6 +289,14 @@ theorem isOption_wf : WellFounded IsOption := by
 
 instance : IsWellFounded _ IsOption := ⟨isOption_wf⟩
 
+theorem IsOption.irrefl (x : IGame) : ¬ IsOption x x := _root_.irrefl x
+
+theorem self_not_mem_leftMoves (x : IGame) : x ∉ x.leftMoves :=
+  fun hx ↦ IsOption.irrefl x (.of_mem_leftMoves hx)
+
+theorem self_not_mem_rightMoves (x : IGame) : x ∉ x.rightMoves :=
+  fun hx ↦ IsOption.irrefl x (.of_mem_rightMoves hx)
+
 /-- **Conway recursion**: build data for a game by recursively building it on its
 left and right sets.
 
@@ -898,6 +906,8 @@ instance : AddLeftReflectLT IGame where
 instance : AddRightReflectLT IGame :=
   addRightReflectLT_of_addLeftReflectLT _
 
+-- TODO: add the general versions of this to Mathlib
+
 theorem add_congr {a b : IGame} (h₁ : a ≈ b) {c d : IGame} (h₂ : c ≈ d) : a + c ≈ b + d :=
   ⟨add_le_add h₁.1 h₂.1, add_le_add h₁.2 h₂.2⟩
 
@@ -906,6 +916,14 @@ theorem add_congr_left {a b c : IGame} (h : a ≈ b) : a + c ≈ b + c :=
 
 theorem add_congr_right {a b c : IGame} (h : a ≈ b) : c + a ≈ c + b :=
   add_congr .rfl h
+
+@[simp]
+theorem add_fuzzy_add_iff_left {a b c : IGame} : a + b ‖ a + c ↔ b ‖ c := by
+  simp [IncompRel]
+
+@[simp]
+theorem add_fuzzy_add_iff_right {a b c : IGame} : b + a ‖ c + a ↔ b ‖ c := by
+  simp [IncompRel]
 
 theorem sub_congr {a b : IGame} (h₁ : a ≈ b) {c d : IGame} (h₂ : c ≈ d) : a - c ≈ b - d :=
   add_congr h₁ (neg_congr h₂)
