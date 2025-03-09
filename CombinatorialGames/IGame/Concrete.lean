7f4237f44dc951e7bf15394b2fa370308fae7378
Tristan F. <LeoDog896@hotmail.com>
2025-03-09 01:38:39 -0800

    style: move concrete to move relations

--- CombinatorialGames/IGame/Concrete.lean
+++ CombinatorialGames/IGame/Concrete.lean
@@ -9,7 +9,7 @@ import CombinatorialGames.IGame.Impartial
 /-!
 # Combinatorial games from a type of states
 
-A "concrete" game is a type of states endowed with well-founded subsequence relations for the
+A "concrete" game is a type of states endowed with well-founded move relations for the
 left and right players. This is often a more convenient representation for a game, which can then be
 used to define a `IGame`.
 -/
@@ -20,38 +20,38 @@ open IGame
 
 variable {α : Type*}
 
-/-- A "concrete" game is a type of states endowed with well-founded subsequence relations for the
+/-- A "concrete" game is a type of states endowed with well-founded move relations for the
 left and right players. -/
 class ConcreteGame (α : Type*) where
-  /-- The subsequence relation for the left player. -/
-  subsequentL : α → α → Prop
-  /-- The subsequence relation for the right player. -/
-  subsequentR : α → α → Prop
-  /-- The subsequence relation is well-founded. -/
-  isWellFounded_subsequent : IsWellFounded α fun a b ↦ subsequentL a b ∨ subsequentR a b
+  /-- The move relation for the left player. -/
+  relLeft : α → α → Prop
+  /-- The move relation for the right player. -/
+  relRight : α → α → Prop
+  /-- The move relation is well-founded. -/
+  isWellFounded_subsequent : IsWellFounded α fun a b ↦ relLeft a b ∨ relRight a b
 
 namespace ConcreteGame
 variable [ConcreteGame α]
 
-local infix:50 " ≺ₗ " => subsequentL
-local infix:50 " ≺ᵣ " => subsequentR
+local infix:50 " ≺ₗ " => relLeft
+local infix:50 " ≺ᵣ " => relRight
 attribute [instance] isWellFounded_subsequent
 
-theorem subrelation_subsequentL :
-    Subrelation subsequentL fun a b : α ↦ subsequentL a b ∨ subsequentR a b :=
+theorem subrelation_relLeft :
+    Subrelation relLeft fun a b : α ↦ relLeft a b ∨ relRight a b :=
   Or.inl
 
-theorem subrelation_subsequentR :
-    Subrelation subsequentR fun a b : α ↦ subsequentL a b ∨ subsequentR a b :=
+theorem subrelation_relRight :
+    Subrelation relRight fun a b : α ↦ relLeft a b ∨ relRight a b :=
   Or.inr
 
-instance [ConcreteGame α] : IsWellFounded α subsequentL := subrelation_subsequentL.isWellFounded
-instance [ConcreteGame α] : IsWellFounded α subsequentR := subrelation_subsequentR.isWellFounded
+instance [ConcreteGame α] : IsWellFounded α relLeft := subrelation_relLeft.isWellFounded
+instance [ConcreteGame α] : IsWellFounded α relRight := subrelation_relRight.isWellFounded
 
 /-- Defines a concrete game from a single relation, to be used for both players. -/
 def ofImpartial (r : α → α → Prop) [h : IsWellFounded α r] : ConcreteGame α where
-  subsequentL := r
-  subsequentR := r
+  relLeft := r
+  relRight := r
   isWellFounded_subsequent := by convert h; rw [or_self]
 
 /-- Turns a state of a `ConcreteGame` into an `IGame`. -/
@@ -82,7 +82,7 @@ theorem mem_rightMoves_toIGame_of_relRight {a b : α} (hab : b ≺ᵣ a) :
   rw [rightMoves_toIGame]
   exact ⟨b, hab, rfl⟩
 
-theorem neg_toIGame (h : subsequentL (α := α) = subsequentR) (a : α) : -toIGame a = toIGame a := by
+theorem neg_toIGame (h : relLeft (α := α) = relRight) (a : α) : -toIGame a = toIGame a := by
   ext
   all_goals
     simp only [leftMoves_neg, rightMoves_neg, rightMoves_toIGame, Set.mem_neg, Set.mem_image,
@@ -94,14 +94,14 @@ theorem neg_toIGame (h : subsequentL (α := α) = subsequentR) (a : α) : -toIGa
 termination_by isWellFounded_subsequent.wf.wrap a
 decreasing_by all_goals aesop
 
-theorem impartial_toIGame (h : subsequentL (α := α) = subsequentR) (a : α) :
+theorem impartial_toIGame (h : relLeft (α := α) = relRight) (a : α) :
     (toIGame a).Impartial := by
   rw [impartial_def, neg_toIGame h, leftMoves_toIGame, rightMoves_toIGame]
   refine ⟨.rfl, fun i hi ↦ ?_, fun i hi ↦ ?_⟩
   all_goals rw [← hi.choose_spec.2]
-  · have := subrelation_subsequentL <| hi.choose_spec.1
+  · have := subrelation_relLeft <| hi.choose_spec.1
     exact impartial_toIGame h _
-  · have := subrelation_subsequentR <| hi.choose_spec.1
+  · have := subrelation_relRight <| hi.choose_spec.1
     exact impartial_toIGame h _
 termination_by isWellFounded_subsequent.wf.wrap a
 
