c082cb7ade8db859c48db6ae1fa8b4efdbd11270
Violeta Hernández <vi.hdz.p@gmail.com>
2025-03-13 06:47:38 -0600

    more progress

--- CombinatorialGames/Surreal/Division.lean
+++ CombinatorialGames/Surreal/Division.lean
@@ -46,18 +46,6 @@ protected theorem IGame.Numeric.mul_pos_of_neg_of_neg {x y : IGame} [Numeric x] 
 
 namespace Surreal.Division
 
-theorem numeric_option_inv {x : IGame} [Numeric x]
-    (hl : ∀ y ∈ x.leftMoves, Numeric y⁻¹) (hr : ∀ y ∈ x.rightMoves, Numeric y⁻¹) :
-    (∀ y ∈ x⁻¹.leftMoves, Numeric y) ∧ (∀ y ∈ x⁻¹.rightMoves, Numeric y) := by
-  apply invRec x Numeric.zero
-  all_goals
-    intro y hy
-    intros
-    first |
-      have := Numeric.of_mem_leftMoves hy; have := hl _ hy |
-      have := Numeric.of_mem_rightMoves hy; have := hr _ hy
-    infer_instance
-
 theorem one_neg_mul_invOption (x : IGame) {y : IGame} (hy : y * y⁻¹ ≈ 1) (a : IGame)
     [Numeric x] [Numeric y] [Numeric y⁻¹] [Numeric a] :
     1 - x * invOption x y a ≈ (1 - x * a) * (y - x) / y := by
@@ -66,9 +54,32 @@ theorem one_neg_mul_invOption (x : IGame) {y : IGame} (hy : y * y⁻¹ ≈ 1) (a
   simp only [one_mul, sub_eq_add_neg, add_mul, hy]
   ring
 
+theorem mulOption_self_inv (x : IGame) {y : IGame} (hy : y * y⁻¹ ≈ 1) (a : IGame)
+    [Numeric x] [Numeric x⁻¹] [Numeric y] [Numeric y⁻¹] [Numeric a] :
+    mulOption x x⁻¹ y a ≈ 1 + (x⁻¹ - invOption x y a) * y := by
+  rw [mul_comm] at hy
+  rw [← Surreal.mk_eq_mk] at *
+  dsimp [mulOption, invOption] at *
+  simp only [sub_eq_add_neg, add_mul, neg_mul, mul_assoc, hy]
+  ring
+
+  #exit
+
+theorem numeric_option_inv {x : IGame} [Numeric x]
+    (hl : ∀ y ∈ x.leftMoves, 0 < y → Numeric y⁻¹) (hr : ∀ y ∈ x.rightMoves, Numeric y⁻¹) :
+    (∀ y ∈ x⁻¹.leftMoves, Numeric y) ∧ (∀ y ∈ x⁻¹.rightMoves, Numeric y) := by
+  apply invRec x Numeric.zero
+  all_goals
+    intro y hy hy'
+    intros
+    first |
+      have := Numeric.of_mem_leftMoves hy; have := hl _ hy hy' |
+      have := Numeric.of_mem_rightMoves hy; have := hr _ hy
+    infer_instance
+
 theorem mul_inv_option_mem (x : IGame) [Numeric x]
-    (hl : ∀ y ∈ x.leftMoves, Numeric y⁻¹) (hr : ∀ y ∈ x.rightMoves, Numeric y⁻¹)
-    (hl' : ∀ y ∈ x.leftMoves, y * y⁻¹ ≈ 1) (hr' : ∀ y ∈ x.rightMoves, y * y⁻¹ ≈ 1) :
+    (hl : ∀ y ∈ x.leftMoves, 0 < y → Numeric y⁻¹) (hr : ∀ y ∈ x.rightMoves, Numeric y⁻¹)
+    (hl' : ∀ y ∈ x.leftMoves, 0 < y → y * y⁻¹ ≈ 1) (hr' : ∀ y ∈ x.rightMoves, y * y⁻¹ ≈ 1) :
     (∀ y ∈ x⁻¹.leftMoves, x * y < 1) ∧ (∀ y ∈ x⁻¹.rightMoves, 1 < x * y) := by
   apply invRec x
   · simp
@@ -83,18 +94,18 @@ theorem mul_inv_option_mem (x : IGame) [Numeric x]
       exact Numeric.lt_rightMove hy
   · intro y hy hy' a ha h
     have := Numeric.of_mem_leftMoves hy
-    have := hl y hy
+    have := hl y hy hy'
     have := (numeric_option_inv hl hr).1 a ha
-    rw [← IGame.sub_pos, (one_neg_mul_invOption x (hl' y hy) a).lt_congr_right]
+    rw [← IGame.sub_pos, (one_neg_mul_invOption x (hl' y hy hy') a).lt_congr_right]
     apply Numeric.mul_pos (Numeric.mul_pos_of_neg_of_neg _ _) (IGame.inv_pos y)
     · rwa [IGame.sub_neg]
     · rw [IGame.sub_neg]
       exact Numeric.leftMove_lt hy
   · intro y hy hy' a ha h
     have := Numeric.of_mem_leftMoves hy
-    have := hl y hy
+    have := hl y hy hy'
     have := (numeric_option_inv hl hr).1 a ha
-    rw [← IGame.sub_neg, (one_neg_mul_invOption x (hl' y hy) a).lt_congr_left]
+    rw [← IGame.sub_neg, (one_neg_mul_invOption x (hl' y hy hy') a).lt_congr_left]
     apply Numeric.mul_neg_of_neg_of_pos (Numeric.mul_neg_of_pos_of_neg _ _) (IGame.inv_pos y)
     · rwa [IGame.sub_pos]
     · rw [IGame.sub_neg]
@@ -131,42 +142,6 @@ theorem one_neg_mul_invOption (x : IGame) {y : IGame} (hy : Game.mk (y * y⁻¹)
   Game.mk_mul_assoc, Game.mk_add_mul, Game.mk_mul_add]-/
 -/
 #exit
-
-private theorem numeric_inv' (x : IGame) [Numeric x] :
-    (∀ y ∈ (x⁻¹).leftMoves, Numeric y) ∧ (∀ y ∈ (x⁻¹).rightMoves, Numeric y) := by
-  refine invRec x ?_ ?_ ?_ ?_ ?_
-  · infer_instance
-  · intro y hy a ha _
-    rw [invOption, IGame.div_eq_mul_inv]
-    have := Numeric.of_mem_rightMoves hy
-    have := numeric_inv' y
-    infer_instance
-  · sorry
-  · sorry
-  · sorry
-termination_by x
-decreasing_by igame_wf
-
-#exit
-private theorem mul_move_inv {x : IGame} [Numeric x] (h : 0 < x) :
-    (∀ y ∈ (x⁻¹).leftMoves, x * y < 1) ∧ (∀ y ∈ (x⁻¹).rightMoves, 1 < x * y) := by
-  refine invRec x ?_ ?_ ?_ ?_ ?_
-  · simp
-  · intro y hy a ha hxa
-    rw [invOption, IGame.div_eq_mul_inv]
-    sorry
-  · sorry
-  · sorry
-  · sorry
-
-#exit
-theorem mul_leftMove_inv_lt {x y : IGame} [Numeric x] (h : 0 < x) (hy : y ∈ (x⁻¹).leftMoves) :
-    x * y < 1 :=
-  (mul_move_inv h).1 y hy
-
-theorem lt_mul_rightMove_inv {x y : IGame} [Numeric x] (h : 0 < x) (hy : y ∈ (x⁻¹).rightMoves) :
-    1 < x * y :=
-  (mul_move_inv h).2 y hy
 
 end IGame
 end
