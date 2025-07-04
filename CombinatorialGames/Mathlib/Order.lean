ad8fc494a0a576f271595cf52982fa55f384b08f
Violeta Hernández <vi.hdz.p@gmail.com>
2025-07-03 20:10:18 -0600

    move

--- CombinatorialGames/Mathlib/Order.lean
+++ CombinatorialGames/Mathlib/Order.lean
@@ -1,5 +1,7 @@
 import Mathlib.Order.Antisymmetrization
 
+/-! ### Basic order theorems -/
+
 theorem not_le_of_le_of_not_le {α : Type*} [Preorder α] {a b c : α} (h₁ : a ≤ b) (h₂ : ¬ c ≤ b) :
     ¬ c ≤ a :=
   fun h ↦ h₂ (h.trans h₁)
@@ -25,3 +27,31 @@ theorem not_antisymmRel_of_gt {α} [Preorder α] {x y : α} : y < x → ¬ Antis
 
 alias LT.lt.not_antisymmRel := not_antisymmRel_of_lt
 alias LT.lt.not_antisymmRel_symm := not_antisymmRel_of_gt
+
+theorem Finset.exists_between {α : Type*} [LinearOrder α] [DenselyOrdered α] {s t : Finset α}
+    (hs : s.Nonempty) (ht : t.Nonempty) (H : ∀ x ∈ s, ∀ y ∈ t, x < y) :
+    ∃ b, (∀ x ∈ s, x < b) ∧ (∀ y ∈ t, b < y) := by
+  convert _root_.exists_between (a₁ := s.max' hs) (a₂ := t.min' ht) (by simp_all) <;> simp
+
+theorem Finset.exists_between' {α : Type*} [LinearOrder α] [DenselyOrdered α] (s t : Finset α)
+    [NoMaxOrder α] [NoMinOrder α] [Nonempty α]
+    (H : ∀ x ∈ s, ∀ y ∈ t, x < y) :
+    ∃ b, (∀ x ∈ s, x < b) ∧ (∀ y ∈ t, b < y) := by
+  by_cases hs : s.Nonempty <;> by_cases ht : t.Nonempty
+  · exact s.exists_between hs ht H
+  · exact let ⟨p, hp⟩ := exists_gt (s.max' hs); ⟨p, by simp_all⟩
+  · exact let ⟨p, hp⟩ := exists_lt (t.min' ht); ⟨p, by simp_all⟩
+  · exact Nonempty.elim ‹_› fun p ↦ ⟨p, by simp_all⟩
+
+theorem Set.Finite.exists_between {α : Type*} [LinearOrder α] [DenselyOrdered α] {s t : Set α}
+    (hsf : s.Finite) (hs : s.Nonempty) (htf : t.Finite) (ht : t.Nonempty)
+    (H : ∀ x ∈ s, ∀ y ∈ t, x < y) :
+    ∃ b, (∀ x ∈ s, x < b) ∧ (∀ y ∈ t, b < y) := by
+  convert Finset.exists_between (s := hsf.toFinset) (t := htf.toFinset)
+    (by simpa) (by simpa) (by simpa) using 1; simp
+
+theorem Set.Finite.exists_between' {α : Type*} [LinearOrder α] [DenselyOrdered α]
+    [NoMaxOrder α] [NoMinOrder α] [Nonempty α] {s t : Set α} (hs : s.Finite) (ht : t.Finite)
+    (H : ∀ x ∈ s, ∀ y ∈ t, x < y) :
+    ∃ b, (∀ x ∈ s, x < b) ∧ (∀ y ∈ t, b < y) := by
+  convert hs.toFinset.exists_between' ht.toFinset (by simpa) using 1; simp
