03cab9039607ad19fc9263b29cbdca73771f038a
Aaron Liu <aaronliu2008@outlook.com>
2025-07-05 17:10:16 -0400

    vague progress

--- CombinatorialGames/Surreal/SignExpansion.lean
+++ CombinatorialGames/Surreal/SignExpansion.lean
@@ -19,29 +19,35 @@ universe u
 noncomputable section
 
 namespace Surreal
-open IGame
+open IGame Game
 
-def signApprox (o : NatOrdinal.{u}) (x : Surreal.{u}) : Surreal :=
-  @Surreal.mk {{s : IGame.{u} | s.birthday < o} ∩ {s | ∃ _ : s.Numeric, .mk s < x} |
-    {s : IGame.{u} | s.birthday < o} ∩ {s | ∃ _ : s.Numeric, x < .mk s}}ᴵ <| by
-      rw [numeric_def]
-      refine ⟨?_, ?_, ?_⟩
-      · intro y hy z hz
-        rw [leftMoves_ofSets] at hy
-        rw [rightMoves_ofSets] at hz
-        have := hy.2.1
-        have := hz.2.1
-        exact Surreal.mk_lt_mk.mp (hy.2.2.trans hz.2.2)
-      · intro y hy
-        rw [leftMoves_ofSets] at hy
-        exact hy.2.1
-      · intro y hy
-        rw [rightMoves_ofSets] at hy
-        exact hy.2.1
+def signApproxIGame (o : NatOrdinal.{u}) (x : Surreal.{u}) : IGame.{u} :=
+  {{s : IGame.{u} | s.birthday < o} ∩ {s | ∃ _ : s.Numeric, .mk s < x} |
+    {s : IGame.{u} | s.birthday < o} ∩ {s | ∃ _ : s.Numeric, x < .mk s}}ᴵ
+
+instance signApproxIGameNumeric (o : NatOrdinal.{u}) (x : Surreal.{u}) :
+    Numeric (x.signApproxIGame o) := by
+  rw [numeric_def, signApproxIGame]
+  refine ⟨?_, ?_, ?_⟩
+  · intro y hy z hz
+    rw [leftMoves_ofSets] at hy
+    rw [rightMoves_ofSets] at hz
+    have := hy.2.1
+    have := hz.2.1
+    exact mk_lt_mk.mp (hy.2.2.trans hz.2.2)
+  · intro y hy
+    rw [leftMoves_ofSets] at hy
+    exact hy.2.1
+  · intro y hy
+    rw [rightMoves_ofSets] at hy
+    exact hy.2.1
+
+def signApprox (o : NatOrdinal.{u}) (x : Surreal.{u}) : Surreal.{u} :=
+  Surreal.mk (x.signApproxIGame o)
 
 theorem birthday_signApprox_le (o : NatOrdinal.{u}) (x : Surreal.{u}) :
     (signApprox o x).toGame.birthday ≤ o := by
-  rw [signApprox, Surreal.toGame_mk]
+  rw [signApprox, toGame_mk, signApproxIGame]
   apply (Game.birthday_mk_le _).trans
   rw [birthday_le_iff, leftMoves_ofSets, rightMoves_ofSets]
   constructor
@@ -50,9 +56,25 @@ theorem birthday_signApprox_le (o : NatOrdinal.{u}) (x : Surreal.{u}) :
   · intro y hy
     exact hy.left
 
+theorem signApprox_of_birthday_le {o : NatOrdinal.{u}} {x : Surreal.{u}}
+    (h : x.toGame.birthday ≤ o) : x.signApprox o = x := by
+  obtain ⟨k, hkg, hkb⟩ := x.toGame.birthday_eq_iGameBirthday
+  rw [← hkb] at h
+  rw [signApprox, ← x.out_eq, Surreal.mk_eq_mk, ← Game.mk_eq_mk,
+    game_out_eq, ← hkg, Game.mk_eq_mk, x.out_eq]
+  apply Fits.equiv_of_forall_birthday_le
+  · rw [signApproxIGame]
+    constructor
+    · intro z hz
+
+      sorry
+    · sorry
+  · sorry
+
 theorem monotone_signApprox {o : NatOrdinal.{u}} : Monotone (signApprox o) := by
   intro x y hxy
-  rw [signApprox, signApprox, @Surreal.mk_le_mk, le_iff_forall_lf]
+  simp_rw [signApprox, Surreal.mk_le_mk, signApproxIGame]
+  rw [le_iff_forall_lf]
   refine ⟨fun z hz => lf_of_le_leftMove le_rfl ?_, fun z hz => lf_of_rightMove_le le_rfl ?_⟩
   · rw [leftMoves_ofSets] at hz ⊢
     exact ⟨hz.1, hz.2.1, lt_of_lt_of_le hz.2.2 hxy⟩
