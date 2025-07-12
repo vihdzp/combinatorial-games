24a9c2ea4b08c6f2b4c8a877012ec1947dcf1fba
Aaron Liu <aaronliu2008@outlook.com>
2025-07-12 16:36:15 -0400

    add minies

--- CombinatorialGames/Game/Order.lean
+++ CombinatorialGames/Game/Order.lean
@@ -17,7 +17,7 @@ universe u
 noncomputable section
 namespace IGame
 
-theorem tiny_lf_of_nonneg_of_forall_neg_le {x a : IGame} (hx : 0 ⧏ x)
+theorem tiny_lf_of_not_nonpos_of_forall_neg_le {x a : IGame} (hx : 0 ⧏ x)
     (ha : ∀ y ∈ x.rightMoves, -a ≤ y) : ⧾a ⧏ x := by
   refine lf_of_rightMove_le ?_ (rightMoves_tiny _ ▸ Set.mem_singleton _)
   rw [le_iff_forall_lf]
@@ -28,16 +28,32 @@ theorem tiny_le_of_pos_of_forall_neg_le {x a : IGame} (hx : 0 < x)
   rw [le_iff_forall_lf]
   constructor
   · simpa using hx.not_ge
-  · exact fun y hy ↦ tiny_lf_of_nonneg_of_forall_neg_le (lf_rightMove_of_le hx.le hy) (ha y hy)
+  · exact fun y hy ↦ tiny_lf_of_not_nonpos_of_forall_neg_le (lf_rightMove_of_le hx.le hy) (ha y hy)
 
 theorem tiny_toIGame_birthday_lt {x : IGame.{u}} (hx : 0 < x) : ⧾x.birthday.toIGame < x := by
   apply lt_of_le_not_ge
   · refine tiny_le_of_pos_of_forall_neg_le hx
       fun y hy z hz ↦ (neg_toIGame_birthday_le z).trans' ?_
     simpa using ((birthday_lt_of_mem_rightMoves hz).trans (birthday_lt_of_mem_rightMoves hy)).le
-  · refine tiny_lf_of_nonneg_of_forall_neg_le hx.not_ge
+  · refine tiny_lf_of_not_nonpos_of_forall_neg_le hx.not_ge
       fun y hy ↦ (neg_toIGame_birthday_le y).trans' ?_
     simpa using (birthday_lt_of_mem_rightMoves hy).le
+
+theorem lf_miny_of_not_nonneg_of_forall_le {x a : IGame} (hx : x ⧏ 0)
+    (ha : ∀ y ∈ x.leftMoves, y ≤ a) : x ⧏ ⧿a := by
+  refine lf_of_le_leftMove ?_ (leftMoves_miny _ ▸ Set.mem_singleton _)
+  rw [le_iff_forall_lf]
+  simpa using ⟨fun z hz ↦ lf_of_le_leftMove (ha z hz) (by simp), hx⟩
+
+theorem le_miny_of_neg_of_forall_le_neg {x a : IGame} (hx : x < 0)
+    (ha : ∀ y ∈ x.leftMoves, ∀ z ∈ y.leftMoves, z ≤ a) : x ≤ ⧿a := by
+  rw [le_iff_forall_lf]
+  constructor
+  · exact fun y hy ↦ lf_miny_of_not_nonneg_of_forall_le (leftMove_lf_of_le hx.le hy) (ha y hy)
+  · simpa using hx.not_ge
+
+theorem lt_miny_toIGame_birthday {x : IGame.{u}} (hx : x < 0) : x < ⧿x.birthday.toIGame :=by
+  simpa using IGame.lt_neg.2 (tiny_toIGame_birthday_lt (IGame.zero_lt_neg.2 hx))
 
 instance : DenselyOrdered IGame.{u} where
   dense a b hab := by
