205711a0706f8db68011934ff5d56667dfd3ef76
Violeta Hernández <vi.hdz.p@gmail.com>
2025-07-15 17:11:20 -0600

    progress

--- CombinatorialGames/Surreal/Cut.lean
+++ CombinatorialGames/Surreal/Cut.lean
@@ -554,9 +554,12 @@ theorem isSmall_infRight (x : IGame) : IsSmall (infRight x) := by
 
 /-! ### Birthday of cuts -/
 
-/-- The birthday of a small cut is defined as the infimum of `⨆ a ∈ s, a.birthday + 1`, over all
-sets `s` of surreals which serve as witnesses for the smallness of `x`. For a non-small cut, we
-default `birthday x = ⊤`.
+/-- The birthday of a small cut `x` is defined as the infimum of `⨆ a ∈ s, a.birthday + 1` over all
+sets `s` of surreals where either the infimum of the left cuts or the supremum of right cuts of `s`
+equals `x`.
+
+The birthday takes values in `WithTop NatOrdinal`. Cuts with birthday `⊤` are exactly those that
+aren't `IsSmall`.
 
 This isn't a term in the literature, but it's useful for proving that birthdays of surreals equal
 those of their associated games. -/
@@ -566,11 +569,8 @@ noncomputable def birthday (x : Cut) : WithTop NatOrdinal :=
 
 theorem birthday_eq_sSup_birthday (x : Cut) :
     ∃ s : Set Surreal, (sInf (leftSurreal '' s) = x ∨ sSup (rightSurreal '' s) = x) ∧
-      sSup ((fun x ↦ (x.birthday : WithTop _) + 1) '' s) = birthday x := by
-  rw [birthday]
-  apply csInf_mem (image_nonempty.2 _)
-
-    #exit
+      sSup ((fun x ↦ (x.birthday : WithTop _) + 1) '' s) = birthday x :=
+  csInf_mem (image_nonempty.2 ⟨_, .inr <| sSup_rightSurreal_left x⟩)
 
 theorem birthday_sInf_leftSurreal_le (s : Set Surreal) :
     (sInf (leftSurreal '' s)).birthday ≤
@@ -592,6 +592,32 @@ theorem birthday_iSup_rightSurreal_le {ι : Type*} (f : ι → Surreal) :
   convert birthday_sSup_rightSurreal_le (range f) <;>
   simp [iSup, ← range_comp']
 
+theorem birthday_sInf_leftSurreal_le' (s : Set Surreal.{u}) [Small.{u} s] :
+    (sInf (leftSurreal '' s)).birthday ≤
+      (sSup ((fun x ↦ x.birthday + 1) '' s) : NatOrdinal) := by
+  apply (birthday_sInf_leftSurreal_le s).trans_eq
+  rw [WithTop.coe_sSup']
+  · simp [image_image]
+  · exact Ordinal.bddAbove_of_small _
+
+theorem birthday_sSup_rightSurreal_le' (s : Set Surreal.{u}) [Small.{u} s] :
+    (sSup (rightSurreal '' s)).birthday ≤
+      (sSup ((fun x ↦ x.birthday + 1) '' s) : NatOrdinal) := by
+  apply (birthday_sSup_rightSurreal_le s).trans_eq
+  rw [WithTop.coe_sSup']
+  · simp [image_image]
+  · exact Ordinal.bddAbove_of_small _
+
+theorem birthday_iInf_leftSurreal_le' {ι : Type*} (f : ι → Surreal.{u}) [Small.{u} ι] :
+    (⨅ i, leftSurreal (f i)).birthday ≤ ⨆ i, (f i).birthday + 1 := by
+  convert birthday_sInf_leftSurreal_le' (range f) <;>
+  simp [iInf, iSup, ← range_comp']
+
+theorem birthday_iSup_rightSurreal_le' {ι : Type*} (f : ι → Surreal.{u}) [Small.{u} ι] :
+    (⨆ i, rightSurreal (f i)).birthday ≤ ⨆ i, (f i).birthday + 1 := by
+  convert birthday_sSup_rightSurreal_le' (range f) <;>
+  simp [iSup, ← range_comp']
+
 @[simp]
 theorem birthday_bot : birthday ⊥ = 0 := by
   simpa using birthday_sSup_rightSurreal_le ∅
@@ -607,5 +633,22 @@ theorem birthday_leftSurreal_le (x : Surreal) : (leftSurreal x).birthday ≤ x.b
 -- TODO: is this an equality?
 theorem birthday_rightSurreal_le (x : Surreal) : (rightSurreal x).birthday ≤ x.birthday + 1 := by
   simpa using birthday_sSup_rightSurreal_le {x}
+
+theorem birthday_lt_top_iff {x : Cut.{u}} : x.birthday < ⊤ ↔ x.IsSmall := by
+  refine ⟨fun hx ↦ ?_, ?_⟩
+  · obtain ⟨o, ho⟩ := WithTop.ne_top_iff_exists.1 hx.ne
+    obtain ⟨s, rfl | rfl, hs⟩ := birthday_eq_sSup_birthday x
+    all_goals
+      suffices Small.{u} s by first | exact .sInf' _ | exact .sSup' _
+      refine small_subset (s := {x : Surreal | x.birthday < o}) fun x hx ↦ ?_
+      rw [← hs] at ho
+      rw [mem_setOf, ← WithTop.coe_lt_coe, ho, lt_sSup_iff]
+      refine ⟨x.birthday + 1, ⟨?_, ?_⟩⟩
+      · use x
+      · convert WithTop.coe_lt_coe.2 <| Order.lt_succ x.birthday
+        simp [Order.succ_eq_add_one]
+  · rintro (s | s)
+    · exact (birthday_sInf_leftSurreal_le' s).trans_lt (WithTop.coe_lt_top _)
+    · exact (birthday_sSup_rightSurreal_le' s).trans_lt (WithTop.coe_lt_top _)
 
 end Cut
