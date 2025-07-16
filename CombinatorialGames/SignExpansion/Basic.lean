fa59a043e02d41f3af1ee325dc7ad429a8001353
Aaron Liu <aaronliu2008@outlook.com>
2025-07-16 10:17:36 -0400

    restrict lemmas + rename size -> length

--- CombinatorialGames/SignExpansion/Basic.lean
+++ CombinatorialGames/SignExpansion/Basic.lean
@@ -84,24 +84,24 @@ theorem sign_eq_coe (e : SignExpansion.{u}) : e.sign = ⇑e := rfl
 theorem apply_eq_zero_of_le {e : SignExpansion} {o o' : Ordinal.{u}}
     (hoo' : o ≤ o') (ho : e o = 0) : e o' = 0 := e.isUpperSet_sign_preimage_singleton_zero hoo' ho
 
-noncomputable def size (e : SignExpansion.{u}) : WithTop NatOrdinal.{u} :=
+noncomputable def length (e : SignExpansion.{u}) : WithTop NatOrdinal.{u} :=
   ⨅ (i : NatOrdinal.{u}) (_ : e i = 0), WithTop.some i
 
-theorem apply_of_size_le {e : SignExpansion.{u}} {o : NatOrdinal.{u}} (h : e.size ≤ o) :
+theorem apply_of_length_le {e : SignExpansion.{u}} {o : NatOrdinal.{u}} (h : e.length ≤ o) :
     e o = 0 := by
-  cases he : e.size with
+  cases he : e.length with
   | top => simp [he] at h
   | coe g =>
     rw [he, WithTop.coe_le_coe, ← lt_succ_iff,
-      ← WithTop.coe_lt_coe, ← he, size] at h
+      ← WithTop.coe_lt_coe, ← he, length] at h
     simp_rw [iInf_lt_iff, WithTop.coe_lt_coe, lt_succ_iff] at h
     obtain ⟨i, hi, hio⟩ := h
     exact e.apply_eq_zero_of_le hio hi
 
 @[simp]
 theorem apply_eq_zero {x : SignExpansion.{u}} {o : NatOrdinal.{u}} :
-    x o = 0 ↔ x.size ≤ o :=
-  ⟨fun h => iInf₂_le o h, apply_of_size_le⟩
+    x o = 0 ↔ x.length ≤ o :=
+  ⟨fun h => iInf₂_le o h, apply_of_length_le⟩
 
 def restrict (x : SignExpansion.{u}) (o : WithTop NatOrdinal.{u}) : SignExpansion.{u} where
   sign i := if i < o then x.sign i else 0
@@ -113,8 +113,8 @@ def restrict (x : SignExpansion.{u}) (o : WithTop NatOrdinal.{u}) : SignExpansio
     exact fun hb => (ha (hab.trans_lt hb)).trans hab
 
 @[simp]
-theorem size_restrict (x : SignExpansion.{u}) (o : WithTop NatOrdinal.{u}) :
-    (x.restrict o).size = min x.size o := by
+theorem length_restrict (x : SignExpansion.{u}) (o : WithTop NatOrdinal.{u}) :
+    (x.restrict o).length = min x.length o := by
   apply eq_of_forall_ge_iff
   intro c
   cases c with
@@ -123,13 +123,22 @@ theorem size_restrict (x : SignExpansion.{u}) (o : WithTop NatOrdinal.{u}) :
     rw [← apply_eq_zero, restrict]
     simp [imp_iff_or_not]
 
-theorem restrict_apply_of_lt {x : SignExpansion.{u}} {o₁ o₂ : NatOrdinal.{u}}
-    (h : o₂ < o₁) : x.restrict o₁ o₂ = x o₂ := by
+theorem restrict_apply_of_coe_lt {x : SignExpansion.{u}} {o₁ : WithTop NatOrdinal.{u}}
+    {o₂ : NatOrdinal.{u}} (h : o₂ < o₁) : x.restrict o₁ o₂ = x o₂ := by
   simp [restrict, h]
 
-theorem restrict_apply_of_le {x : SignExpansion.{u}} {o₁ o₂ : NatOrdinal.{u}}
-    (h : o₁ ≤ o₂) : x.restrict o₁ o₂ = 0 := by
+theorem restrict_apply_of_le_coe {x : SignExpansion.{u}} {o₁ : WithTop NatOrdinal.{u}}
+    {o₂ : NatOrdinal.{u}} (h : o₁ ≤ o₂) : x.restrict o₁ o₂ = 0 := by
   simp [restrict, h]
+
+theorem restrict_of_length_le {e : SignExpansion.{u}} {o : WithTop NatOrdinal.{u}}
+    (ho : e.length ≤ o) : e.restrict o = e := by
+  ext o'
+  obtain ho' | ho' := lt_or_ge (.some o') e.length
+  · rw [restrict_apply_of_coe_lt (ho'.trans_le ho)]
+  · rw [apply_of_length_le ho']
+    apply apply_of_length_le
+    simp [ho']
 
 instance linearOrder : LinearOrder SignExpansion.{u} :=
   LinearOrder.lift' (toLex ⇑·) (by simp [Function.Injective])
