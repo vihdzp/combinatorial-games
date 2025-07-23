5e7bb9154072378ee65b6ba86944e5c8b759067c
Tristan F.-R. <pub.tristanf@gmail.com>
2025-07-23 08:45:22 -0700

    style: use correct import sort
    Co-authored-by: Violeta Hernández <vi.hdz.p@gmail.com>

--- CombinatorialGames/Game/Computability/FGame.lean
+++ CombinatorialGames/Game/Computability/FGame.lean
@@ -3,12 +3,11 @@ Copyright (c) 2025 Violeta Hernández Palacios. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Violeta Hernández Palacios, Kim Morrison
 -/
+import CombinatorialGames.Game.Short
+import CombinatorialGames.Mathlib.Finlift
 import Mathlib.Data.Countable.Small
 import Mathlib.Data.Fintype.Quotient
 import Mathlib.Data.Multiset.Sort
-
-import CombinatorialGames.Game.Short
-import CombinatorialGames.Mathlib.Finlift
 
 /-!
 # Computably short games
