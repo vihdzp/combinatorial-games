1546eb921db55471ef98e5c8fc64cb4dd407d2a5
Violeta Hernández <vi.hdz.p@gmail.com>
2025-08-06 06:06:25 -0600

    outcome branch

--- CombinatorialGames/Game/Loopy/Outcome.lean
+++ CombinatorialGames/Game/Loopy/Outcome.lean
@@ -1,8 +1,24 @@
 import CombinatorialGames.Game.Loopy.Basic
+import CombinatorialGames.Game.IGame
 
 namespace LGame
 
-def LeftStrategy {x : LGame} (f : {y // Subposition y x} → Option {y // Subposition y x}) : Prop :=
+/-- A surviving strategy for Left, going second.
 
+This is a set of states containing `x`, such that for every move Right makes, Left can bring it back
+to the set. -/
+def LeftStrategy (x : LGame) (s : Set LGame) : Prop :=
+  x ∈ s ∧ ∀ y ∈ s, ∀ z ∈ y.rightMoves, ∃ r ∈ z.leftMoves, r ∈ s
+
+/-- A surviving strategy for Right, going second.
+
+This is a set of states containing `x`, such that for every move Left makes, Right can bring it back
+to the set. -/
+def RightStrategy (x : LGame) (s : Set LGame) : Prop :=
+  x ∈ s ∧ ∀ y ∈ s, ∀ z ∈ y.leftMoves, ∃ r ∈ z.rightMoves, r ∈ s
+
+private theorem left_or_right_survive_left (x : LGame) :
+    (∃ y ∈ x.leftMoves, ∃ s, LeftStrategy y s) ∨ ∃ s, RightStrategy x s := by
+  sorry
 
 end LGame
