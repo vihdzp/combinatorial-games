0348aeb88deca514629e41b06574a34de11afa8d
Django Peeters <django.peeters1906@gmail.com>
2025-08-16 17:51:48 +0200

    state helpful theorem

--- CombinatorialGames/Nimber/WWW.lean
+++ CombinatorialGames/Nimber/WWW.lean
@@ -16,21 +16,25 @@ import Mathlib.SetTheory.Ordinal.CantorNormalForm
 # w^(w^w) is algebraically closed
 
 This file aims to prove that `w ^ (w ^ w)` is an algebraically closed field.
+A possible follow-up is to prove that arithmetic in `w ^ (w ^ w)` is effectively computable.
 
-The proof follows [On Numbers And Games][conway2001] (pp. 59-61)
-and [On The Algebraic Closure Of Two][lenstra1977] (pp. 1-8).
+The proof follows [On Numbers And Games][conway2001] (pp. 59-61),
+[On The Algebraic Closure Of Two][lenstra1977] (pp. 1-8) and
+[Combinatorial Games][siegel2013] (pp. 444-450).
 
 ## Todo
 
 Formalize pp. 59-61.
-
 Formalize pp. 1-8.
+Formalize pp.444-450.
 -/
+
+universe u
 
 open Ordinal
 open Nat (nth)
 
-/-! ### Mathlib lemmas -/
+/-! ### Prime lemmas -/
 
 theorem one_lt_nth_prime (k : ℕ) : 1 < nth Nat.Prime k := by
   apply lt_of_lt_of_le (b := 2) (by norm_num)
@@ -46,22 +50,11 @@ noncomputable section
 
 namespace Nimber
 
-protected def field_two : Subfield Nimber := Subfield.closure ∅
+/- First we prove the algebraic closure. -/
 
-private def tau : Nimber := of (omega0 ^ (omega0 ^ omega0))
-
-protected def degree (x : Nimber) : ℕ :=
-  (minpoly Nimber.field_two x).natDegree
-
-/- `kappa q` where `q = (nth Nat.Prime k) ^ (n + 1)` -/
-protected def kappa_q (k n : ℕ) : Nimber :=
-  of <| 2 ^ (omega0 ^ k * (nth Nat.Prime k) ^ n)
-
-protected def kappa (h : ℕ) : Nimber :=
-  sInf {x | x < tau ∧ h ∣ Nimber.degree x}
-
-protected def alpha (h : ℕ) : Nimber :=
-  sInf {x | x < Nimber.kappa h ∧ ∀ y < Nimber.kappa h, y.val ^ h ≠ x.val}
+/-- `kappa_q` where `q = (nth Nat.Prime k) ^ (n + 1)` -/
+protected def kappa_q (k n : ℕ) : Nimber.{u} :=
+  of <| 2 ^ (ω ^ k * (nth Nat.Prime k) ^ n)
 
 protected theorem kappa_q_lt_iff_pair_lt (k n k' n' : ℕ) :
   Nimber.kappa_q k n < Nimber.kappa_q k' n' ↔ k < k' ∨ k = k' ∧ n < n' := by
@@ -103,27 +96,55 @@ protected theorem kappa_q_lt_iff_pair_lt (k n k' n' : ℕ) :
       repeat rw [← opow_natCast, ← natCast_opow]
       rwa [Nat.cast_lt, Nat.pow_lt_pow_iff_right (one_lt_nth_prime k)]
 
+/-- `alpha_p` where `p = nth Nat.Prime (k + 1)` -/
+protected def alpha_p (k : ℕ) : Nimber.{u} :=
+  sInf {x | x < Nimber.kappa_q (k + 1) 0 ∧
+    ∀ y < Nimber.kappa_q (k + 1) 0, y ^ (nth Nat.Prime (k + 1)) ≠ x}
+
+@[simp]
+protected theorem alpha_p_zero : Nimber.alpha_p 0 = ∗2 := by
+  sorry
+
+theorem omega0_cubed_eq_three : (∗ω) ^ 3 = ∗2 := by
+  sorry
+
+/-- Theorem 4.5 on p. 446. -/
+protected theorem kappa_q_pow_reduce :
+  ∀ (k : ℕ), (Nimber.kappa_q.{u} (k + 1) 0) ^ (nth Nat.Prime (k + 1)) = Nimber.alpha_p.{u} k ∧
+  (∀ (n : ℕ), (Nimber.kappa_q.{u} (k + 1) (n + 1)) ^ (nth Nat.Prime (k + 1)) =
+  (Nimber.kappa_q.{u} (k + 1) n) ∧ IsField (Nimber.kappa_q.{u} (k + 1) n)) ∧
+  IsNthDegreeClosed (nth Nat.Prime (k + 1)) (Nimber.kappa_q.{u} (k + 2) 0) := by
+  intro k
+  induction k with
+  | zero =>
+    simp [Nimber.kappa_q]
+    rw [opow_omega0 (by simp) (by exact nat_lt_omega0 2)]
+    refine ⟨omega0_cubed_eq_three, ?_, ?_⟩
+    · sorry
+    · sorry
+  | succ k ih =>
+    sorry
+
+protected theorem kappa_q_pow_eq_alpha (k : ℕ) :
+  (Nimber.kappa_q.{u} (k + 1) 0) ^ (nth Nat.Prime (k + 1)) =
+  Nimber.alpha_p.{u} k := by
+  exact Nimber.kappa_q_pow_reduce k |>.1
+
+protected theorem kappa_q_pow_decrease (k n : ℕ) :
+  (Nimber.kappa_q.{u} (k + 1) (n + 1)) ^ (nth Nat.Prime (k + 1)) =
+  (Nimber.kappa_q.{u} (k + 1) n) := by
+  exact Nimber.kappa_q_pow_reduce k |>.2.1 n |>.1
+
 /-- Add two ordinals as nimbers. -/
 scoped notation:65 x:65 "+ₙᵢₘ" y:66 => val (∗x + ∗y)
 
-protected theorem base_two_expansion (x : Nimber) :
+protected theorem base_two_expansion (x : Nimber.{u}) :
   (CNF 2 x.val).foldr (fun p r ↦ 2 ^ p.1 * p.2 +ₙᵢₘ r) 0 = x.val := by
   sorry
 
-protected theorem kappa_nth_prime_pow (k n : ℕ) {h : n ≠ 0} :
-  Nimber.kappa ((nth Nat.Prime k) ^ n) = Nimber.kappa_q k n := by
-  sorry
+private def tau : Nimber.{u} := ∗(ω ^ (ω ^ ω))
 
-protected theorem kappa_pow_eq_alpha (k : ℕ) :
-  (Nimber.kappa (nth Nat.Prime k)) ^ (nth Nat.Prime k) = Nimber.alpha (nth Nat.Prime k) := by
-  sorry
-
-protected theorem kappa_pow_decrease (k n : ℕ) {h : n ≠ 0} :
-  (Nimber.kappa ((nth Nat.Prime k) ^ (n + 1))) ^ (nth Nat.Prime k)
-  = (Nimber.kappa ((nth Nat.Prime k) ^ n)) := by
-  sorry
-
-private def tau' : Nimber :=
+private def tau' : Nimber.{u} :=
   sInf {x | IsAlgClosed x}
 
 private theorem tau'_eq_tau : tau' = tau := by
@@ -131,7 +152,21 @@ private theorem tau'_eq_tau : tau' = tau := by
 
 /-- public name is more explicit -/
 theorem wpow_wpow_w_first_isAlgClosed :
-  sInf {x | IsAlgClosed x} = (of <| omega0 ^ (omega0 ^ omega0)) := by exact tau'_eq_tau
+  sInf {x | IsAlgClosed x} = ∗(ω ^ (ω ^ ω)) := by exact tau'_eq_tau
+
+/- Then we prove effective arithmetic. -/
+
+protected def field_two : Subfield Nimber := Subfield.closure ∅
+
+protected def degree (x : Nimber.{u}) : ℕ :=
+  (minpoly Nimber.field_two x).natDegree
+
+protected def kappa (h : ℕ) : Nimber.{u} :=
+  sInf {x | x < tau ∧ h ∣ Nimber.degree x}
+
+protected theorem kappa_q_of_kappa (k n : ℕ) :
+  Nimber.kappa ((nth Nat.Prime k) ^ (n + 1)) = Nimber.kappa_q k n := by
+  sorry
 
 end Nimber
 
