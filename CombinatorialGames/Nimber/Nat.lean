1cfe6b00720ae653128acdc9532c153bb6834799
Violeta Hernández <vi.hdz.p@gmail.com>
2025-08-19 17:01:39 -0600

    continue

--- CombinatorialGames/Nimber/Nat.lean
+++ CombinatorialGames/Nimber/Nat.lean
@@ -108,58 +108,9 @@ theorem splitRec_mul_add {motive : NatNimber → Sort*} (zero : motive 0) (one :
   · have := two_le_pow_pow_mul_add t b ha₀
     aesop
 
-/-- Build data by induction on `max m.log2.log2 n.log2.log2`. -/
-def splitRec₂ {motive : NatNimber → NatNimber → Sort*}
-    (zero_zero : motive 0 0) (zero_one : motive 0 1) (one_zero : motive 1 0) (one_one : motive 1 1)
-    (ind : ∀ t, (∀ m < 2 ^ 2 ^ t, ∀ n < 2 ^ 2 ^ t, motive (of m) (of n)) →
-      ∀ a < 2 ^ 2 ^ t, ∀ b < 2 ^ 2 ^ t, ∀ c < 2 ^ 2 ^ t, ∀ d < 2 ^ 2 ^ t, a ≠ 0 ∨ c ≠ 0 →
-      motive (of (2 ^ 2 ^ t * a + b)) (of (2 ^ 2 ^ t * c + d))) (m n : NatNimber) : motive m n :=
-  if h₀₀ : m = 0 ∧ n = 0 then cast (congrArg₂ motive h₀₀.1.symm h₀₀.2.symm) zero_zero else
-  if h₀₁ : m = 0 ∧ n = 1 then cast (congrArg₂ motive h₀₁.1.symm h₀₁.2.symm) zero_one else
-  if h₁₀ : m = 1 ∧ n = 0 then cast (congrArg₂ motive h₁₀.1.symm h₁₀.2.symm) one_zero else
-  if h₁₁ : m = 1 ∧ n = 1 then cast (congrArg₂ motive h₁₁.1.symm h₁₁.2.symm) one_one else by
-  let M := max m.val n.val
-  have h₂ : 2 ≤ M := by sorry
-  have Hm : of (_ + _) = m := Nat.div_add_mod m.val (2 ^ 2 ^ M.log2.log2)
-  have Hn : of (_ + _) = n := Nat.div_add_mod n.val (2 ^ 2 ^ M.log2.log2)
-  apply cast (congrArg₂ motive Hm Hn) (ind ..)
-  · exact fun k hk ↦ splitRec₂ zero one ind k
-  · exact div_pow_pow_lt
-  · exact div_pow_pow_ne_zero h₂
-  · exact Nat.mod_lt _ (pow_pos zero_lt_two _)
-termination_by n
-decreasing_by exact lt_of_lt_pow_pow h₂ hk
+instance : Add NatNimber where
+  add m n := of (m.val ^^^ n.val)
 
-#exit
 
-@[simp]
-theorem splitRec_zero {motive : NatNimber → Sort*} (zero : motive 0) (one : motive 1)
-    (ind : ∀ t, (∀ k < 2 ^ 2 ^ t, motive (of k)) → ∀ a < 2 ^ 2 ^ t, a ≠ 0 → ∀ b < 2 ^ 2 ^ t,
-      motive (of (2 ^ 2 ^ t * a + b))) : splitRec zero one ind 0 = zero := by
-  rw [splitRec]
-  simp
-
-@[simp]
-theorem splitRec_one {motive : NatNimber → Sort*} (zero : motive 0) (one : motive 1)
-    (ind : ∀ t, (∀ k < 2 ^ 2 ^ t, motive (of k)) → ∀ a < 2 ^ 2 ^ t, a ≠ 0 → ∀ b < 2 ^ 2 ^ t,
-      motive (of (2 ^ 2 ^ t * a + b))) : splitRec zero one ind 1 = one := by
-  rw [splitRec]
-  simp
-
-theorem splitRec_mul_add {motive : NatNimber → Sort*} (zero : motive 0) (one : motive 1)
-    (ind : ∀ t, (∀ k < 2 ^ 2 ^ t, motive (of k)) → ∀ a < 2 ^ 2 ^ t, a ≠ 0 → ∀ b < 2 ^ 2 ^ t,
-      motive (of (2 ^ 2 ^ t * a + b))) {a b t : ℕ}
-    (ha : a < 2 ^ 2 ^ t) (ha₀ : a ≠ 0) (hb : b < 2 ^ 2 ^ t) :
-    splitRec zero one ind (of (2 ^ 2 ^ t * a + b)) =
-    ind t (fun k _ ↦ splitRec zero one ind k) a ha ha₀ b hb := by
-  have h := log2_log2_mul_add ha ha₀ hb
-  rw [splitRec, dif_neg, dif_neg, cast_eq_iff_heq]
-  congr!
-  · rw [val_of, h, Nat.mul_add_div, Nat.div_eq_of_lt hb, add_zero]
-    exact pow_pos zero_lt_two _
-  · rw [val_of, h, Nat.mul_add_mod, Nat.mod_eq_of_lt hb]
-  · simp_all
-  · have := two_le_pow_pow_mul_add t b ha₀
-    aesop
 
 end NatNimber
