58c12eef9b9267e3bf8fa64e0690223401ce3d58
Violeta Hernández <vi.hdz.p@gmail.com>
2025-08-20 12:55:11 -0600

    feat: nim operations on naturals (#204)
    Co-authored-by: Tristan F.-R. <pub.tristanf@gmail.com>

---
+++ CombinatorialGames/Nimber/Nat.lean
@@ -0,0 +1,97 @@
+/-
+Copyright (c) 2025 Violeta Hernández Palacios. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Violeta Hernández Palacios
+-/
+import CombinatorialGames.Nimber.Basic
+
+/-!
+# Finite nimber arithmetic
+
+This file defines the type alias `NatNimber` of the natural numbers, endowed with nimber arithmetic.
+The goal is to define the field structure computably, and prove that it matches that on `Nimber`.
+-/
+
+alias! /-- The natural numbers, endowed with nim operations. -/ NatNimber Nat
+
+namespace NatNimber
+
+instance : LinearOrder NatNimber := Nat.instLinearOrder
+
+instance : Lean.ToExpr NatNimber where
+  toExpr x := .app (.const `NatNimber.of []) (Lean.toExpr x.val)
+  toTypeExpr := .const `NatNimber []
+
+instance : ToString NatNimber where
+  toString x := "∗" ++ x.val.repr
+
+@[simp] theorem lt_one_iff_zero {a : NatNimber} : a < 1 ↔ a = 0 := Nat.lt_one_iff
+@[simp] theorem one_le_iff_ne_zero {a : NatNimber} : 1 ≤ a ↔ a ≠ 0 := Nat.one_le_iff_ne_zero
+theorem le_one_iff {a : NatNimber} : a ≤ 1 ↔ a = 0 ∨ a = 1 := Nat.le_one_iff_eq_zero_or_eq_one
+
+/-- The embedding `NatNimber ↪o Nimber`. -/
+def toNimber : NatNimber ↪o Nimber where
+  toFun x := .of x.val
+  inj' x y := by simp
+  map_rel_iff' := by simp
+
+@[simp] theorem toNimber_zero : toNimber 0 = 0 := rfl
+@[simp] theorem toNimber_one : toNimber 1 = 1 := by simp [toNimber]
+@[simp] theorem toNimber_of (n : ℕ) : toNimber (of n) = Nimber.of n := rfl
+
+instance : Neg NatNimber where
+  neg n := n
+
+instance : Add NatNimber where
+  add m n := of (m.val ^^^ n.val)
+
+instance : Sub NatNimber where
+  sub m n := m + n
+
+@[simp]
+theorem toNimber_add (x y : NatNimber) : toNimber (x + y) = .of x.val + .of y.val :=
+  (Nimber.add_nat ..).symm
+
+@[simp] theorem sub_eq (x y : NatNimber) : x - y = x + y := rfl
+@[simp] theorem neg_eq (x : NatNimber) : -x = x := rfl
+
+/-- Multiplies `x` by `∗2 ^ (2 ^ t - 1)`, whenever `x < ∗2 ^ 2 ^ t`. This is a subroutine needed to
+multiply two general nimbers. This makes use of the formula:
+
+$$(a2^{2^t}+b)2^{2^{t+1}-1} = (a2^{2^t-1})2^{2^t-1}+((a+b)2^{2^t-1})2^{2^t}$$
+
+for $a, b < \ast 2 ^ {2 ^ t}$. -/
+private def mulHalf (t : ℕ) (x : NatNimber) : NatNimber :=
+  match t with
+  | 0 => x
+  | t + 1 =>
+    let a := of (x.val / 2 ^ 2 ^ t)
+    let b := of (x.val % 2 ^ 2 ^ t)
+    of ((mulHalf t (a + b)).val * 2 ^ 2 ^ t) + mulHalf t (mulHalf t a)
+
+/-- Multiplies `x` by `y`, whenever `x, y < ∗2 ^ 2 ^ t`. This makes use of the formula:
+
+$$(a2^{2^t}+b)(c2^{2^t}+d)=(ac)2^{2^t-1}+(ac+ad+bc)2^{2^t}+bd$$
+
+for $a, b, c, d < \ast 2 ^ {2 ^ t}$.-/
+private def mul (t : ℕ) (x y : NatNimber) : NatNimber :=
+  match t with
+  | 0 => if x = 0 then 0 else if y = 0 then 0 else 1
+  | t + 1 =>
+    let a := of (x.val / 2 ^ 2 ^ t)
+    let b := of (x.val % 2 ^ 2 ^ t)
+    let c := of (y.val / 2 ^ 2 ^ t)
+    let d := of (y.val % 2 ^ 2 ^ t)
+    let z := mul t a c
+    mulHalf t z + of ((z + mul t a d + mul t b c).val * 2 ^ 2 ^ t) + mul t b d
+
+-- TODO: prove correctness
+instance : Mul NatNimber where
+  mul x y := mul (max x.val.log2.log2 y.val.log2.log2 + 1) x y
+
+end NatNimber
+
+example : Nimber.of 3 + Nimber.of 5 = Nimber.of 6 := by
+  have : NatNimber.of 3 + NatNimber.of 5 = NatNimber.of 6 := by decide
+  apply_fun NatNimber.toNimber at this
+  simpa
