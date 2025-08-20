934e93f29a2394d362230ac59e0279074ef9200e
Violeta Hernández <vi.hdz.p@gmail.com>
2025-08-19 20:52:35 -0600

    mini diff

--- CombinatorialGames/Tactic/OrdinalAlias.lean
+++ CombinatorialGames/Tactic/OrdinalAlias.lean
@@ -3,8 +3,6 @@ Copyright (c) 2025 Violeta Hernández Palacios. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Violeta Hernández Palacios
 -/
-import Mathlib.Data.Nat.Lattice
-import Mathlib.Order.Interval.Finset.Nat
 import Mathlib.SetTheory.Ordinal.Family
 
 /-!
