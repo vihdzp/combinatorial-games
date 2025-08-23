d5ab28eb2f08ed12a8f49f2f8af915be1a0d1d48
Violeta Hernández <vi.hdz.p@gmail.com>
2025-08-22 20:32:39 -0600

    new file

--- CombinatorialGames/Game/Basic.lean
+++ CombinatorialGames/Game/Basic.lean
@@ -142,7 +142,6 @@ instance : CharZero Game := AddMonoidWithOne.toCharZero
 theorem mk_mul_add (x y z : IGame) : mk (x * (y + z)) = mk (x * y) + mk (x * z) := by
   rw [← mk_add, add_eq' (x * y), mul_eq']
   simp only [moves_add, moves_mul, prod_union, union_assoc, image_image, image_union, mk_ofSets']
-  dsimp only [leftMoves, rightMoves, Player.neg_left, Player.neg_right]
   congr! 2
   ext p
   nth_rewrite 2 [union_left_comm]
@@ -180,8 +179,8 @@ theorem _root_.Set.prod_image_right {α β γ : Type*} (f : α → γ) (s : Set 
 set_option maxHeartbeats 500000 in
 theorem mk_mul_assoc (x y z : IGame) : mk (x * y * z) = mk (x * (y * z)) := by
   rw [mul_eq', mul_eq' x (y * z)]
-  simp only [moves_mul, union_prod, prod_union, union_assoc, image_image, image_union, mk_ofSets']
-  dsimp only [leftMoves, rightMoves, Player.neg_left, Player.neg_right]
+  simp only [moves_mul, union_prod, prod_union, union_assoc, image_image, image_union, mk_ofSets',
+    Player.neg_left, Player.neg_right]
   congr! 2
   ext
   nth_rewrite 2 [union_left_comm]
@@ -202,13 +201,13 @@ decreasing_by igame_wf
 theorem lf_ofSets_of_mem_left {s t : Set Game.{u}} [Small.{u} s] [Small.{u} t] {x : Game.{u}}
     (h : x ∈ s) : x ⧏ !{s | t} := by
   rw [ofSets_cases]
-  have : x.out ∈ !{out '' s | out '' t}.leftMoves := by simpa using mem_image_of_mem _ h
+  have : x.out ∈ !{out '' s | out '' t}ᴸ := by simpa using mem_image_of_mem _ h
   simpa [← mk_le_mk] using leftMove_lf this
 
 theorem ofSets_lf_of_mem_right {s t : Set Game.{u}} [Small.{u} s] [Small.{u} t] {x : Game.{u}}
     (h : x ∈ t) : !{s | t} ⧏ x := by
   rw [ofSets_cases]
-  have : x.out ∈ !{out '' s | out '' t}.rightMoves := by simpa using mem_image_of_mem _ h
+  have : x.out ∈ !{out '' s | out '' t}ᴿ := by simpa using mem_image_of_mem _ h
   simpa [← mk_le_mk] using lf_rightMove this
 
 end Game
