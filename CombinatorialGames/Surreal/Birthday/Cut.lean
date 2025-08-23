d1074bc2fefc22f4b8068c804d82f59b38f81da3
Violeta Hernández <vi.hdz.p@gmail.com>
2025-08-22 21:40:41 -0600

    chore: symmetrize `IGame.birthday` API (#215)

--- CombinatorialGames/Surreal/Birthday/Cut.lean
+++ CombinatorialGames/Surreal/Birthday/Cut.lean
@@ -241,12 +241,12 @@ private theorem birthday_game_le (x : IGame) :
     rw [supLeft, iSup_subtype']
     apply (birthday_iSup_le _).trans <| iSup_le fun i ↦ (birthday_game_le _).2.2.2.trans ?_
     rw [← WithTop.coe_add_one, WithTop.coe_le_coe, Order.add_one_le_iff]
-    exact IGame.birthday_lt_of_mem_leftMoves i.2
+    exact IGame.birthday_lt_of_mem_moves i.2
   have H₂ : (infRight x).birthday ≤ x.birthday := by
     rw [infRight, iInf_subtype']
     apply (birthday_iInf_le _).trans <| iSup_le fun i ↦ (birthday_game_le _).2.2.1.trans ?_
     rw [← WithTop.coe_add_one, WithTop.coe_le_coe, Order.add_one_le_iff]
-    exact IGame.birthday_lt_of_mem_rightMoves i.2
+    exact IGame.birthday_lt_of_mem_moves i.2
   use H₁, H₂
   obtain h | h := lt_or_ge (supLeft x) (infRight x)
   · rw [← simplestBtwn_supLeft_infRight h, leftGame_toGame, rightGame_toGame]
