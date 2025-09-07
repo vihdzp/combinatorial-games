4aae8c2cc8e19e7ef25d494984289fc6d88420a9
Violeta Hernández <vi.hdz.p@gmail.com>
2025-09-07 02:06:09 -0600

    Update Basic.lean

--- CombinatorialGames/Nimber/SimplestExtension/Basic.lean
+++ CombinatorialGames/Nimber/SimplestExtension/Basic.lean
@@ -505,7 +505,7 @@ theorem IsField.of_le_one {x : Nimber} (h : x ≤ 1) : IsField x := by
 
 theorem IsField.mul_eq_of_lt {x y z : Nimber} (hx : IsRing x) (hy : IsField y)
     (hyx : y ≤ x) (hzy : z < y) : x *ₒ z = x * z :=
-  hx.mul_eq_of_lt' hy.toIsGroup hyx hzy fun _ hw ↦ (hy.inv_lt hw).trans_le hyx
+  hx.mul_eq_of_lt hy.toIsGroup hyx hzy fun _ hw ↦ (hy.inv_lt hw).trans_le hyx
 
 /-- A version of `IsField.mul_eq_of_lt` stated in terms of `Ordinal`. -/
 theorem IsField.mul_eq_of_lt' {x y z : Ordinal} (hx : IsRing (∗x)) (hy : IsField (∗y))
