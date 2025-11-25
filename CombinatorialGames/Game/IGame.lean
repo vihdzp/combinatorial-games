2a1c1454c703a182612cc4c118575227b68df71a
Violeta Hernández <vi.hdz.p@gmail.com>
2025-11-25 07:09:20 -0600

    remove redundant theorems

--- CombinatorialGames/Game/IGame.lean
+++ CombinatorialGames/Game/IGame.lean
@@ -279,10 +279,6 @@ instance : Zero IGame := ⟨!{fun _ ↦ ∅}⟩
 
 theorem zero_def : (0 : IGame) = !{fun _ ↦ ∅} := rfl
 
-@[game_cmp] theorem leftMoves_zero : 0ᴸ = ∅ := moves_ofSets ..
-@[game_cmp] theorem rightMoves_zero : 0ᴿ = ∅ := moves_ofSets ..
-
--- TODO: remove the former?
 @[simp, game_cmp] theorem moves_zero (p : Player) : moves p 0 = ∅ := moves_ofSets ..
 
 instance : Inhabited IGame := ⟨0⟩
