b6113f6d455aa27db8f9b1ba6380a6c3e9797ae1
Violeta Hernández <vi.hdz.p@gmail.com>
2025-11-27 03:09:54 -0600

    Spell name correctly (sorry)

--- CombinatorialGames/Tactic/AddInstances.lean
+++ CombinatorialGames/Tactic/AddInstances.lean
@@ -1,7 +1,7 @@
 /-
-Copyright (c) 2025 Frantisek Silvasi. All rights reserved.
+Copyright (c) 2025 František Silváši. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Frantisek Silvasi
+Authors: František Silváši
 -/
 import Lean.Elab.Tactic.Meta
 
