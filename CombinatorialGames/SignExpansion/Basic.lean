214884080a408b4c7ca6814705a0e021f21c5e8b
Violeta Hernández Palacios <vi.hdz.p@gmail.com>
2025-12-30 17:28:55 -0600

    feat: sign expansions are a complete lattice (#282)
    Co-authored-by: Aaron Liu <aaronliu2008@outlook.com>

--- CombinatorialGames/SignExpansion/Basic.lean
+++ CombinatorialGames/SignExpansion/Basic.lean
@@ -4,9 +4,10 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Aaron Liu, Violeta Hernández Palacios
 -/
 import CombinatorialGames.NatOrdinal.Basic
+import Mathlib.Algebra.Group.Pointwise.Set.Basic
 import Mathlib.Data.Fintype.Order
 import Mathlib.Data.Sign.Defs
-import Mathlib.Order.PiLex
+import Mathlib.Order.CompleteLattice.PiLex
 
 /-!
 # Sign expansions
@@ -20,6 +21,59 @@ to the development of surreal numbers.
 -/
 
 universe u
+
+/-! ### For Mathlib -/
+
+attribute [grind =] Pi.toLex_apply
+
+instance : ZeroLEOneClass SignType where
+  zero_le_one := by decide
+
+@[local simp← ]
+theorem Set.preimage_neg {α ι : Type*} [InvolutiveNeg α] (f : ι → α) {s : Set α} :
+    f ⁻¹' (-s) = (-f) ⁻¹' s :=
+  rfl
+
+@[simp]
+theorem Pi.Lex.neg_apply {α β : Type*} [Neg β] (x : Lex (α → β)) (i : α) : (-x) i = -x i :=
+  rfl
+
+-- TODO: we're missing an `AntitoneNeg` typeclass to express the following theorems generally.
+
+theorem Pi.Lex.neg_lt_neg {α : Type*} [LinearOrder α] [WellFoundedLT α]
+    {x y : Lex (α → SignType)} (h : x < y) : -y < -x := by
+  obtain ⟨i, hi⟩ := h
+  use i
+  simp_all
+
+@[simp]
+theorem Pi.Lex.neg_lt_neg_iff {α : Type*} [LinearOrder α] [WellFoundedLT α]
+    {x y : Lex (α → SignType)} : -x < -y ↔ y < x where
+  mp := by simpa using @Pi.Lex.neg_lt_neg _ _ _ (-x) (-y)
+  mpr := Pi.Lex.neg_lt_neg
+
+@[simp]
+theorem Pi.Lex.neg_le_neg_iff {α : Type*} [LinearOrder α] [WellFoundedLT α]
+    {x y : Lex (α → SignType)} : -x ≤ -y ↔ y ≤ x := by
+  simp [← not_lt]
+
+theorem Pi.Lex.neg_lt_iff {α : Type*} [LinearOrder α] [WellFoundedLT α]
+    {x y : Lex (α → SignType)} : -x < y ↔ -y < x := by
+  simpa using Pi.Lex.neg_lt_neg_iff (y := -y)
+
+theorem Pi.Lex.lt_neg_iff {α : Type*} [LinearOrder α] [WellFoundedLT α]
+    {x y : Lex (α → SignType)} : x < -y ↔ y < -x := by
+  simpa using Pi.Lex.neg_lt_neg_iff (x := -x)
+
+theorem Pi.Lex.neg_le_iff {α : Type*} [LinearOrder α] [WellFoundedLT α]
+    {x y : Lex (α → SignType)} : -x ≤ y ↔ -y ≤ x := by
+  simpa using Pi.Lex.neg_le_neg_iff (y := -y)
+
+theorem Pi.Lex.le_neg_iff {α : Type*} [LinearOrder α] [WellFoundedLT α]
+    {x y : Lex (α → SignType)} : x ≤ -y ↔ y ≤ -x := by
+  simpa using Pi.Lex.neg_le_neg_iff (x := -x)
+
+/-! ### Sign expansions -/
 
 noncomputable section
 
@@ -42,12 +96,27 @@ instance : FunLike SignExpansion NatOrdinal SignType where
 /-- Every sign after the first `0` is also `0`. -/
 theorem isUpperSet_preimage_singleton_zero (x : SignExpansion) : IsUpperSet (x ⁻¹' {0}) := x.2
 
-@[simp] theorem coe_mk (f h) : SignExpansion.mk f h = f := rfl
+/-- Adjust definitional equalities of a sign expansion. -/
+def copy (x : SignExpansion) (sign : NatOrdinal → SignType)
+    (h : sign = x) : SignExpansion where
+  sign
+  isUpperSet_preimage_singleton_zero' := h ▸ isUpperSet_preimage_singleton_zero x
+
+@[simp]
+theorem copy_eq (x : SignExpansion) (sign : NatOrdinal → SignType)
+    (h : sign = x) : x.copy sign h = x :=
+  DFunLike.coe_injective h
+
+@[simp] theorem coe_mk (f h) : mk f h = f := rfl
 @[simp] theorem sign_eq_coe (x : SignExpansion) : x.sign = ⇑x := rfl
 
 @[ext]
 protected theorem ext {x y : SignExpansion} (hxy : ∀ o, x o = y o) : x = y :=
   DFunLike.coe_injective (funext hxy)
+
+@[simp]
+theorem mk_eq_mk {f g h₁ h₂} : mk f h₁ = mk g h₂ ↔ f = g := by
+  simp [DFunLike.ext'_iff]
 
 theorem apply_eq_zero_of_le {x : SignExpansion} {o o' : Ordinal}
     (hoo' : o ≤ o') (ho : x o = 0) : x o' = 0 :=
@@ -109,15 +178,11 @@ theorem length_top : length ⊤ = ⊤ := by
   simp [length]
 
 instance : Neg SignExpansion where
-  neg e :=
-  { sign := -e
-    isUpperSet_preimage_singleton_zero' a b hab ha := by
-      simp only [Set.mem_preimage, Pi.neg_apply, Set.mem_singleton_iff,
-        SignType.neg_eq_zero_iff, apply_eq_zero] at ha ⊢
-      exact ha.trans (WithTop.coe_le_coe.2 hab) }
+  neg e := ⟨-e, by simpa using e.2⟩
 
 @[simp] theorem coe_neg (x : SignExpansion) : ⇑(-x : SignExpansion) = -⇑x := rfl
 theorem neg_apply (x : SignExpansion) (o : NatOrdinal) : (-x) o = -x o := rfl
+@[simp] theorem neg_mk (f h) : -mk f h = mk (-f) (by simpa) := rfl
 
 @[simp] theorem neg_zero : -(0 : SignExpansion) = 0 := rfl
 @[simp] theorem neg_bot : -(⊥ : SignExpansion) = ⊤ := rfl
@@ -177,6 +242,9 @@ theorem restrict_top_right {x : SignExpansion} : x ↾ ⊤ = x := by
 
 /-! ### Order structure -/
 
+-- This has deliberately not been upstreamed to Mathlib.
+instance : CompleteLinearOrder SignType := Fintype.toCompleteLinearOrder _
+
 instance : LinearOrder SignExpansion :=
   LinearOrder.lift' (toLex ⇑·) (by simp [Function.Injective])
 
@@ -187,18 +255,278 @@ instance : BoundedOrder SignExpansion where
   le_top _ := le_iff_toLex.2 le_top
   bot_le _ := le_iff_toLex.2 bot_le
 
-protected theorem neg_lt_neg {x y : SignExpansion} (h : x < y) : -y < -x := by
-  obtain ⟨i, hi⟩ := h
-  use i
-  simp_all
+protected theorem neg_lt_neg {x y : SignExpansion} (h : x < y) : -y < -x :=
+  Pi.Lex.neg_lt_neg h
 
 @[simp]
-protected theorem neg_lt_neg_iff {x y : SignExpansion} : -x < -y ↔ y < x where
-  mp := by simpa using SignExpansion.neg_lt_neg (x := -x) (y := -y)
-  mpr := SignExpansion.neg_lt_neg
+protected theorem neg_lt_neg_iff {x y : SignExpansion} : -x < -y ↔ y < x :=
+  Pi.Lex.neg_lt_neg_iff
 
 @[simp]
 protected theorem neg_le_neg_iff {x y : SignExpansion} : -x ≤ -y ↔ y ≤ x :=
-  le_iff_le_iff_lt_iff_lt.2 SignExpansion.neg_lt_neg_iff
+  Pi.Lex.neg_le_neg_iff
+
+protected theorem neg_le {x y : SignExpansion} : -x ≤ y ↔ -y ≤ x :=
+  by simpa using SignExpansion.neg_le_neg_iff (y := -y)
+
+protected theorem le_neg {x y : SignExpansion} : x ≤ -y ↔ y ≤ -x :=
+  by simpa using SignExpansion.neg_le_neg_iff (x := -x)
+
+protected theorem neg_lt {x y : SignExpansion} : -x < y ↔ -y < x :=
+  by simpa using SignExpansion.neg_lt_neg_iff (y := -y)
+
+protected theorem lt_neg {x y : SignExpansion} : x < -y ↔ y < -x :=
+  by simpa using SignExpansion.neg_lt_neg_iff (x := -x)
+
+/-! #### Floor function -/
+
+open Classical in
+/-- The floor function on a function `NatOrdinal → SignType` "rounds" it downwards to the nearest
+valid `SignExpansion`. -/
+def floor (f : NatOrdinal → SignType) : SignExpansion :=
+  if hf : IsUpperSet (f ⁻¹' {0}) then ⟨f, hf⟩ else
+    let a := sInf (f ⁻¹' {0})
+    haveI ha b : b < a → f b ≠ 0 := notMem_of_lt_csInf'
+    { sign c := if c < a then f c else
+        if f (sInf {b | a < b ∧ f b ≠ 0}) = -1 then
+          if c = a then -1 else 1
+        else 0
+      isUpperSet_preimage_singleton_zero' := by
+        by_cases h : f (sInf {b | a < b ∧ f b ≠ 0}) = -1
+        · convert isUpperSet_empty
+          aesop
+        · convert isUpperSet_Ici a
+          aesop }
+
+private theorem floor_of_eq_neg_one {f : NatOrdinal → SignType} (hf : ¬ IsUpperSet (f ⁻¹' {0}))
+    (hf' : f (sInf {b | sInf (f ⁻¹' {0}) < b ∧ f b ≠ 0}) = -1) (c : NatOrdinal) :
+    floor f c = if c < sInf (f ⁻¹' {0}) then f c else if c = sInf (f ⁻¹' {0}) then -1 else 1 := by
+  simp_all [floor]
+
+private theorem floor_of_eq_one {f : NatOrdinal → SignType} (hf : ¬ IsUpperSet (f ⁻¹' {0}))
+    (hf' : f (sInf {b | sInf (f ⁻¹' {0}) < b ∧ f b ≠ 0}) = 1) (c : NatOrdinal) :
+    floor f c = if c < sInf (f ⁻¹' {0}) then f c else 0 := by
+  simp_all [floor]
+
+private theorem nonempty_of_not_isUpperSet {f : NatOrdinal → SignType}
+    (hf : ¬ IsUpperSet (f ⁻¹' {0})) :
+    (f ⁻¹' {0}).Nonempty ∧ {b | sInf (f ⁻¹' {0}) < b ∧ f b ≠ 0}.Nonempty := by
+  constructor
+  · contrapose! hf
+    rw [hf]
+    exact isUpperSet_empty
+  · simp_rw [IsUpperSet, Set.mem_preimage, Set.mem_singleton_iff, not_forall] at hf
+    obtain ⟨a, b, hab, ha, hb⟩ := hf
+    obtain rfl | hab := hab.eq_or_lt
+    · contradiction
+    · exact ⟨b, hab.trans_le' <| csInf_le' ha, hb⟩
+
+theorem floor_of_isUpperSet {f : NatOrdinal → SignType} (hf : IsUpperSet (f ⁻¹' {0})) :
+    floor f = ⟨f, hf⟩ :=
+  dif_pos hf
+
+@[simp]
+theorem floor_coe (x : SignExpansion) : floor x = x :=
+  floor_of_isUpperSet x.2
+
+theorem floor_apply_of_lt_sInf {f : NatOrdinal → SignType} {a : NatOrdinal}
+    (hf : a < sInf (f ⁻¹' {0})) : floor f a = f a := by
+  rw [floor]
+  split_ifs
+  · rfl
+  · simp [hf.not_ge]
+
+theorem floor_lt_of_not_isUpperSet {f : NatOrdinal → SignType} (hf : ¬ IsUpperSet (f ⁻¹' {0})) :
+    toLex ⇑(floor f) < toLex f := by
+  obtain ⟨hf₁, hf₂⟩ := nonempty_of_not_isUpperSet hf
+  have hf' := csInf_mem hf₁
+  rw [floor, dif_neg hf]
+  dsimp
+  split_ifs with h
+  · use sInf (f ⁻¹' {0})
+    simp_all
+  · refine ⟨sInf {b | sInf (f ⁻¹' {0}) < b ∧ f b ≠ 0}, ?_, ?_⟩
+    · simp_rw [Pi.toLex_apply, ite_eq_left_iff, not_lt]
+      intro a ha ha'
+      by_contra ha₀
+      obtain rfl | ha' := ha'.eq_or_lt
+      · simp_all
+      · exact notMem_of_lt_csInf' ha ⟨ha', Ne.symm ha₀⟩
+    · dsimp
+      rw [if_neg]
+      · obtain h | h | h := (f (sInf {b | sInf (f ⁻¹' {0}) < b ∧ f b ≠ 0})).trichotomy
+        · contradiction
+        · cases (csInf_mem hf₂).2 h
+        · exact h ▸ zero_lt_one
+      · exact (csInf_mem hf₂).1.not_gt
+
+theorem floor_le (f : NatOrdinal → SignType) : toLex ⇑(floor f) ≤ toLex f := by
+  by_cases hf : IsUpperSet (f ⁻¹' {0})
+  · rw [floor_of_isUpperSet hf]; rfl
+  · exact (floor_lt_of_not_isUpperSet hf).le
+
+theorem floor_lt {f : NatOrdinal → SignType} {x : SignExpansion} :
+    floor f < x ↔ toLex f < toLex ⇑x := by
+  by_cases hf : IsUpperSet (f ⁻¹' {0})
+  · rw [floor_of_isUpperSet hf]
+    rfl
+  obtain ⟨hf₁, hf₂⟩ := nonempty_of_not_isUpperSet hf
+  have hf₁' := csInf_mem hf₁
+  have hf₂' := csInf_mem hf₂
+  refine ⟨fun ⟨a, ha⟩ ↦ ?_, (floor_le f).trans_lt⟩
+  dsimp at ha
+  obtain ha' | ha' := lt_or_ge a (sInf (f ⁻¹' {0}))
+  · rw [floor_apply_of_lt_sInf ha'] at ha
+    exact ⟨a, fun b hb ↦ (floor_apply_of_lt_sInf (hb.trans ha')).symm.trans (ha.1 _ hb), ha.2⟩
+  · obtain h | h | h := (f (sInf {b | sInf (f ⁻¹' {0}) < b ∧ f b ≠ 0})).trichotomy
+    · simp_rw [floor_of_eq_neg_one hf h, if_neg ha'.not_gt] at ha
+      obtain rfl | ha' := ha'.eq_or_lt
+      · rw [if_pos rfl] at ha
+        simp +contextual only [↓reduceIte] at ha
+        obtain hx | hx | hx := (x (sInf (f ⁻¹' {0}))).trichotomy
+        · simp [hx] at ha
+        · refine ⟨sInf {b | sInf (f ⁻¹' {0}) < b ∧ f b ≠ 0}, fun b hb ↦ ?_, ?_⟩
+          · obtain hb' | hb' := lt_or_ge b (sInf (f ⁻¹' {0}))
+            · exact ha.1 b hb'
+            · apply Eq.trans _ (x.isUpperSet_preimage_singleton_zero hb' hx).symm
+              by_contra
+              refine notMem_of_lt_csInf' hb ⟨hb'.lt_of_ne ?_, this⟩
+              grind
+          · dsimp
+            rw [h, SignType.neg_one_lt_iff]
+            exact (x.isUpperSet_preimage_singleton_zero hf₂'.1.le hx).ge
+        · use sInf (f ⁻¹' {0})
+          simp_all
+      · rw [if_neg ha'.ne'] at ha
+        simpa using ha.2
+    · cases hf₂'.2 h
+    · simp_rw [floor_of_eq_one hf h, if_neg ha'.not_gt] at ha
+      obtain rfl | ha' := ha'.eq_or_lt
+      · use sInf (f ⁻¹' {0})
+        simp_all
+      · apply (mt (x.isUpperSet_preimage_singleton_zero ha'.le) ha.2.ne' _).elim
+        simpa using (ha.1 _ ha').symm
+
+theorem le_floor {f : NatOrdinal → SignType} {x : SignExpansion} :
+    x ≤ floor f ↔ toLex ⇑x ≤ toLex f := by
+  simpa using floor_lt.not
+
+theorem gc_coe_floor : GaloisConnection (toLex ∘ (⇑·) : SignExpansion → _) (floor ∘ ofLex) :=
+  fun _ _ ↦ le_floor.symm
+
+/-- `floor` as a Galois coinsertion. -/
+def gciFloor : GaloisCoinsertion (toLex ∘ (⇑·) : SignExpansion → _) (floor ∘ ofLex) where
+  gc := gc_coe_floor
+  u_l_le := by simp
+  choice x h := (floor (ofLex x)).copy (ofLex x) (toLex_inj.1 (h.antisymm (gc_coe_floor.l_u_le x)))
+  choice_eq x h := copy_eq ..
+
+/-! #### Ceiling function -/
+
+/-- The ceiling function on a function `NatOrdinal → SignType` "rounds" it upwards to the nearest
+valid `SignExpansion`. -/
+def ceil (f : NatOrdinal → SignType) : SignExpansion :=
+  -floor (-f)
+
+theorem ceil_of_isUpperSet {f : NatOrdinal → SignType} (hf : IsUpperSet (f ⁻¹' {0})) :
+    ceil f = ⟨f, hf⟩ := by
+  rw [ceil, floor_of_isUpperSet (by simpa)]
+  simp
+
+@[simp]
+theorem ceil_coe (x : SignExpansion) : ceil x = x :=
+  ceil_of_isUpperSet x.2
+
+theorem ceil_apply_of_lt_sInf {f : NatOrdinal → SignType} {a : NatOrdinal}
+    (hf : a < sInf (f ⁻¹' {0})) : ceil f a = f a := by
+  rw [ceil, neg_apply, floor_apply_of_lt_sInf]
+  · simp
+  · simpa
+
+theorem lt_ceil_of_not_isUpperSet {f : NatOrdinal → SignType} (h : ¬ IsUpperSet (f ⁻¹' {0})) :
+    toLex f < toLex ⇑(ceil f) := by
+  rw [ceil]
+  simpa [Pi.Lex.lt_neg_iff] using floor_lt_of_not_isUpperSet (f := -f) (by simpa)
+
+theorem le_ceil (f : NatOrdinal → SignType) : toLex f ≤ toLex ⇑(ceil f) := by
+  rw [ceil]
+  simpa [Pi.Lex.le_neg_iff] using floor_le (-f)
+
+theorem lt_ceil {f : NatOrdinal → SignType} {x : SignExpansion} :
+    x < ceil f ↔ toLex ⇑x < toLex f := by
+  rw [ceil, SignExpansion.lt_neg, floor_lt]
+  simp
+
+theorem ceil_le {f : NatOrdinal → SignType} {x : SignExpansion} :
+    ceil f ≤ x ↔ toLex f ≤ toLex ⇑x := by
+  simpa using lt_ceil.not
+
+theorem gc_ceil_coe : GaloisConnection (ceil ∘ ofLex) (toLex ∘ (⇑·) : SignExpansion → _) :=
+  fun _ _ ↦ ceil_le
+
+/-- `ceil` as a Galois insertion. -/
+def giCeil : GaloisInsertion (ceil ∘ ofLex) (toLex ∘ (⇑·) : SignExpansion → _) where
+  gc := gc_ceil_coe
+  le_l_u := by simp
+  choice x h := (ceil (ofLex x)).copy (ofLex x) (toLex_inj.1 ((gc_ceil_coe.le_u_l x).antisymm h))
+  choice_eq x h := copy_eq ..
+
+@[simp]
+theorem floor_neg (f : NatOrdinal → SignType) : floor (-f) = -ceil f := by
+  simp [ceil]
+
+@[simp]
+theorem ceil_neg (f : NatOrdinal → SignType) : ceil (-f) = -floor f := by
+  simp [ceil]
+
+theorem floor_le_ceil (f : NatOrdinal → SignType) : floor f ≤ ceil f :=
+  (floor_le f).trans (le_ceil f)
+
+theorem floor_lt_ceil_of_not_isUpperSet {f : NatOrdinal → SignType} (h : ¬ IsUpperSet (f ⁻¹' {0})) :
+    floor f < ceil f :=
+  (floor_lt_of_not_isUpperSet h).trans_le (le_ceil f)
+
+/-! #### Complete linear order instance -/
+
+instance : CompleteLattice SignExpansion :=
+  fast_instance%
+  { __ := instLinearOrder.toBiheytingAlgebra _
+    __ := giCeil.liftCompleteLattice.toCompleteSemilatticeInf
+    __ := gciFloor.liftCompleteLattice.toCompleteSemilatticeSup }
+
+instance : CompleteLinearOrder SignExpansion where
+  __ := LinearOrder.toBiheytingAlgebra _
+  __ := instCompleteLattice
+  __ := instLinearOrder
+
+theorem coe_sInf (s : Set SignExpansion) :
+    sInf s = ofLex (sInf ((fun x : SignExpansion ↦ toLex ⇑x) '' s)) := rfl
+
+theorem coe_iInf {ι} (f : ι → SignExpansion) :
+    ⨅ i, f i = ofLex (⨅ i : ι, toLex (⇑(f i))) := by
+  rw [iInf, coe_sInf]
+  congr
+  aesop
+
+theorem sInf_apply (s : Set SignExpansion) (i : NatOrdinal) :
+    sInf s i = ⨅ x : {x : SignExpansion // x ∈ s ∧ ∀ j < i, x j = sInf s j}, x.1 i := by
+  rw [coe_sInf, Pi.ofLex_apply, Pi.Lex.sInf_apply]
+  apply congrArg sInf
+  aesop
+
+theorem coe_sSup (s : Set SignExpansion) :
+    sSup s = ofLex (sSup ((fun x : SignExpansion ↦ toLex ⇑x) '' s)) := rfl
+
+theorem coe_iSup {ι} (f : ι → SignExpansion) :
+    ⨆ i, f i = ofLex (⨆ i : ι, toLex (⇑(f i))) := by
+  rw [iSup, coe_sSup]
+  congr
+  aesop
+
+theorem sSup_apply (s : Set SignExpansion) (i : NatOrdinal) :
+    sSup s i = ⨆ x : {x : SignExpansion // x ∈ s ∧ ∀ j < i, x j = sSup s j}, x.1 i := by
+  rw [coe_sSup, Pi.ofLex_apply, Pi.Lex.sSup_apply]
+  apply congrArg sSup
+  aesop
 
 end SignExpansion
