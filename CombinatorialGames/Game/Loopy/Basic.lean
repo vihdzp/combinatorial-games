1d334ebc235a6127736ac072c5004b3a0f00d568
vihdzp <vi.hdz.p@gmail.com>
2026-02-12 23:48:55 -0600

    no-expose ofsets

--- CombinatorialGames/Game/Loopy/Basic.lean
+++ CombinatorialGames/Game/Loopy/Basic.lean
@@ -293,6 +293,7 @@ theorem corec_moves_apply (x : LGame) : corec moves x = x := by simp
 
 It's not possible to create a non-well-founded game through this constructor alone. For that,
 see `LGame.corec`. -/
+@[no_expose]
 instance : OfSets LGame.{u} fun _ ↦ True where
   ofSets lr _ :=
     have this (p) : ∀ (a : Option LGame),
