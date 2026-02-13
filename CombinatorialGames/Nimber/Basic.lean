25df8144c5bc15a4a58455e5047fe4161798fe5e
vihdzp <vi.hdz.p@gmail.com>
2026-02-12 19:08:06 -0600

    tweak

--- CombinatorialGames/Nimber/Basic.lean
+++ CombinatorialGames/Nimber/Basic.lean
@@ -66,14 +66,14 @@ theorem not_small_nimber : ¬ Small.{u} Nimber.{u} := not_small_ordinal
 
 variable {a b c : Nimber.{u}}
 
-/-- Nimber addition is recursively defined so that `a + b` is the smallest nimber not equal to
-`a' + b` or `a + b'` for `a' < a` and `b' < b`. -/
 -- We write the binders like this so that the termination checker works.
-protected def add (a b : Nimber.{u}) : Nimber.{u} :=
+private def add (a b : Nimber.{u}) : Nimber.{u} :=
   sInf {x | (∃ a', ∃ (_ : a' < a), Nimber.add a' b = x) ∨
     ∃ b', ∃ (_ : b' < b), Nimber.add a b' = x}ᶜ
 termination_by (a, b)
 
+/-- Nimber addition is recursively defined so that `a + b` is the smallest nimber not equal to
+`a' + b` or `a + b'` for `a' < a` and `b' < b`. -/
 instance : Add Nimber :=
   ⟨Nimber.add⟩
 
@@ -217,11 +217,10 @@ theorem range_natCast : Set.range ((↑) : ℕ → Nimber) = {0, 1} := by
   · use 0; simp
   · use 1; simp
 
-@[game_cmp]
 theorem natCast_eq_mod (n : ℕ) : (n : Nimber) = (n % 2 : ℕ) := by
   simp [natCast_eq_if, Nat.even_iff]
 
-@[simp, game_cmp]
+@[simp]
 theorem ofNat_eq_mod (n : ℕ) [n.AtLeastTwo] : (ofNat(n) : Nimber) = (n % 2 : ℕ) :=
   natCast_eq_mod n
 
@@ -234,14 +233,10 @@ theorem range_intCast : Set.range ((↑) : ℤ → Nimber) = {0, 1} := by
   · use 0; simp
   · use 1; simp
 
-@[game_cmp]
 theorem intCast_eq_mod (n : ℤ) : (n : Nimber) = (n % 2 : ℤ) := by
   simp [intCast_eq_if, Int.even_iff]
 
 end Mathlib
-
--- This lets `game_cmp` reduce any instances of `NatCast`.
-attribute [game_cmp] Nat.reduceMod
 
 @[simp]
 theorem add_cancel_right (a b : Nimber) : a + b + b = a := by
