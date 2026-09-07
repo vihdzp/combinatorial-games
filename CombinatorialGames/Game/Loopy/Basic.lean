bb863d3deb779f709920996cf5ab674d8655083b
vihdzp <vi.hdz.p@gmail.com>
2026-09-07 00:18:23 -0600

    Fix linter

--- CombinatorialGames/Game/Loopy/Basic.lean
+++ CombinatorialGames/Game/Loopy/Basic.lean
@@ -20,8 +20,8 @@ import Mathlib.Data.Countable.Small
 
 The standard notion of a game studied in combinatorial game theory is that of a terminating game,
 meaning that there exists no infinite sequence of moves. Loopy games relax this condition by
-allowing "self-referential" games, with the basic examples being `on = {on | }`, `off = { | off}`, and
-`dud = {dud | dud}`.
+allowing "self-referential" games, with the basic examples being `on = {on | }`, `off = { | off}`,
+and `dud = {dud | dud}`.
 
 In the literature, loopy games are defined as rooted directed graphs up to isomorphism. However,
 it's simpler to define `LGame` as the coinductive type for the single constructor:
