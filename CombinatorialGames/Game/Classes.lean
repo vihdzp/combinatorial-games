ddd1b8c7ecfd3163d2158f801a0587501934972a
Laurance <60111599+LLaurance@users.noreply.github.com>
2026-09-15 19:57:31 +0800

    Comparison of numeric games with zero

--- CombinatorialGames/Game/Classes.lean
+++ CombinatorialGames/Game/Classes.lean
@@ -418,6 +418,9 @@ theorem lt_iff_exists_le [Numeric x] [Numeric y] :
     x < y ↔ (∃ z ∈ yᴸ, x ≤ z) ∨ (∃ z ∈ xᴿ, z ≤ y) := by
   rw [← Numeric.not_le, lf_iff_exists_le]
 
+theorem pos_iff_exists_le [Numeric x] : 0 < x ↔ ∃ z ∈ xᴸ, 0 ≤ z := by
+  simpa using lt_iff_exists_le (x := 0)
+
 theorem left_lt [Numeric x] (h : y ∈ xᴸ) : y < x := by
   numeric; simpa using left_lf h
 
@@ -434,6 +437,9 @@ protected instance neg (x : IGame) [Numeric x] : Numeric (-x) := by
     simpa using Numeric.neg y
 termination_by x
 decreasing_by igame_wf
+
+theorem neg_iff_exists_le [Numeric x] : x < 0 ↔ ∃ z ∈ xᴿ, z ≤ 0 := by
+  simpa using lt_iff_exists_le (y := 0)
 
 @[simp]
 theorem neg_iff {x : IGame} : Numeric (-x) ↔ Numeric x :=
